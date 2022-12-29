4a0ea2700b7e76b9cf4af5c4e97032c8ccd16fc4
Willy Antunez <willyantunezg@gmail.com>
2022-12-29 00:24:37 -0600

    data: archivo data creado con skills

---
+++ src/data/data.js
@@ -0,0 +1,41 @@
+export const data = {
+    skills: {
+        tech: [
+            {
+                name: 'HTML5',
+                domainPercent: 90, 
+            },
+            {
+                name: 'CSS',
+                domainPercent: 95, 
+            },
+            {
+                name: 'Javascript',
+                domainPercent: 95, 
+            },
+            {
+                name: 'SCSS',
+                domainPercent: 80, 
+            },
+            {
+                name: 'React.js',
+                domainPercent: 50, 
+            },
+            {
+                name: 'Vue2',
+                domainPercent: 50, 
+            },
+            {
+                name: 'Git y Github',
+                domainPercent: 80, 
+            },
+        ],
+        soft: [
+            { name: 'Resolución de problemas', },
+            { name: 'Trabajo en equipo', },
+            { name: 'Comunicación', },
+            { name: 'Responsabilidad', },
+            { name: 'Creatividad', },
+        ]
+    }
+}
