be92052b8c32b64d3938144d03a6e1f82993bdf4
Willy Antunez <willyantunezg@gmail.com>
2023-01-04 20:00:08 -0600

    Data: proyectos añadidos a la data

--- src/data/data.js
+++ src/data/data.js
@@ -41,5 +41,27 @@ export const data = {
             { name: 'Responsabilidad', },
             { name: 'Creatividad', },
         ]
-    }
+    },
+    projects: [
+        {
+            title: 'Pokedex: by Willy Antunez',
+            description: 'Mi propio sitio web que usa los datos de la "PokeAPI" para mostrar información  de todos los Pokémon',
+            id: 'pokedex',
+        },
+        {
+            title: 'Numbers to text',
+            description: 'Pagina web desarrollada con Javascript puro en la que se ingresa un numero entero de hasta 30 dígitos con hasta 12 dígitos decimales y lo convierte a texto.',
+            id: 'numbers-to-text',
+        },
+        {
+            title: 'Diputados Honduras',
+            description: 'Pagina en Vue.js que recolectaba datos de la API del CNE durante las elecciones Honduras y mostraba un resumen (Actualmente no funcionando, API fuera de servicio)',
+            id: 'diputados-honduras',
+        },
+        {
+            title: 'Where in the world',
+            description: 'Pagina en Vue.js que recolectaba datos sobre paises de la API restcountries.com y los muestra',
+            id: 'where-in-the-world',
+        },
+    ]
 }
