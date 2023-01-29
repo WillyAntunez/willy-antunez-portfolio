eade911d16d1346eaa29f6ecb552c1109948d1c3
Willy Antunez <willyantunezg@gmail.com>
2023-01-29 10:00:17 -0600

    Contact: form handle and validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,10 +9,59 @@ import whatsappIcon from '../assets/img/whatsapp-icon.png';
 import facebookIcon from '../assets/svg/facebook.svg';
 import linkedinIcon from '../assets/svg/linkedin.svg';
 import twitterIcon from '../assets/svg/twitter.svg';
+import { useForm } from '../hooks/useForm';
+
+const formConfig = [
+  {
+    name: 'name',
+    regexValidators: [
+      [/\S+/, 'Este campo no puede estar vacío'],
+      [/^[a-zA-Z\s]*$/, 'Ingresaste caracteres no validos'],
+      [/^((?!asdasd).)*$/, '¿Estás escribiendo caracteres al azar?'],
+    ]
+  },
+  {
+    name: 'email',
+    regexValidators: [
+      [/\S+/, 'Este campo no puede estar vacío'],
+      [
+        /^(([^<>()[\]\\.,;:\s@”]+(\.[^<>()[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/, 
+        'Ingresaste un correo no válido'
+      ],
+    ]
+  },
+  {
+    name: 'subject',
+    regexValidators: [
+      [/\S+/, 'Este campo no puede estar vacío'],
+      [/^.{0,50}$/, 'El máximo de caracteres son 50'],
+      [/^((?!asdasd).)*$/, '¿Estás escribiendo caracteres al azar?'],
+    ],
+  },
+  {
+    name: 'msgBody',
+    regexValidators: [
+      [/\S+/, 'Este campo no puede estar vacío'],
+      [/^[\s\S]{0,5000}$/, 'Tampoco me contés tu vida... se más breve por favor.'],
+    ]
+  }
+]
 
 export const Contact = ({id}) => {
 
   const {social} = useContext(ProfileContext)
+  
+  const {name, email, subject, msgBody, onInputChange, isFormValid, setFormSubmitted, wasFormSubmited} = useForm(formConfig);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted();
+
+    if(isFormValid){
+      // TODO: enviar email
+    }
+  }
+
 
   return (
     <>
@@ -20,7 +69,6 @@ export const Contact = ({id}) => {
             <div className="contact">
 
               <h2 className='contact__title'> Contáctame </h2>  
-              {/* <hr className='contact__separator' /> */}
 
               <div className='contact__way contact__way--centered contact__way--first'>
                 <h3 className='contact__subtitle'> ¡Enviame un Whatsapp!</h3>
@@ -47,25 +95,65 @@ export const Contact = ({id}) => {
 
               <div className="contact__way contact__way--form">
                 <h3 className='contact__subtitle'> ¡Usa mi formulario de contacto!</h3>
-                <form className="contact__form">
+                <form className="contact__form" onSubmit={onFormSubmit} noValidate>
                   <div className="contact__formGroup contact__formGroup--wrapable">
                     <label className='contact__text contact__label'><b>Tu nombre:</b></label>
-                    <input className='contact__input contact' type="text" placeholder='Ej: Alan Turing' />
+                    <input 
+                      className={`contact__input contact ${(wasFormSubmited && name.notValid)? 'contact__input--notValid' : ''}`} 
+                      type="text" 
+                      placeholder='Ej: Alan Turing'
+                      value={name.value} 
+                      name="name"
+                      onChange={onInputChange}
+                    />
+                    {
+                      (wasFormSubmited && name.notValid) && <small className='contact__inputError'>{name.notValid}</small>
+                    }
                   </div>
 
                   <div className="contact__formGroup contact__formGroup--wrapable">
                     <label className='contact__text contact__label'><b>Correo electrónico:</b></label>
-                    <input className='contact__input contact' type="text" placeholder='Ej: Alan_turing@enigma.com' />
+                    <input 
+                      type="text" 
+                      placeholder='Ej: Alan_turing@enigma.com'
+                      className={`contact__input contact ${(wasFormSubmited && email.notValid)? 'contact__input--notValid' : ''}`} 
+                      value={email.value}
+                      name="email"
+                      onChange={onInputChange}
+                    />
+                    {
+                      (wasFormSubmited && email.notValid) && <small className='contact__inputError'>{email.notValid}</small>
+                    }
                   </div>
                   
                   <div className="contact__formGroup">
                     <label className='contact__text contact__label'><b> Asunto: </b></label>
-                    <input className='contact__input' type="text" placeholder='¿Por qué tema me contactas?' />
+                    <input 
+                      type="text" 
+                      placeholder='¿Por qué tema me contactas?'
+                      className={`contact__input contact ${(wasFormSubmited && subject.notValid)? 'contact__input--notValid' : ''}`} 
+                      value={subject.value}
+                      name="subject"
+                      onChange={onInputChange} 
+                    />
+                    {
+                      (wasFormSubmited && subject.notValid) && <small className='contact__inputError'>{subject.notValid}</small>
+                    }
                   </div>
                   
                   <div className="contact__formGroup">
                     <label className='contact__text contact__label'> <b> Mensaje: </b> </label>
-                    <textarea className='contact__input' placeholder='Escribe aquí lo que me quieras decir' rows={8}></textarea>
+                    <textarea 
+                      placeholder='Escribe aquí lo que me quieras decir' 
+                      rows={8}
+                      className={`contact__input contact ${(wasFormSubmited && msgBody.notValid)? 'contact__input--notValid' : ''}`} 
+                      value={msgBody.value}
+                      name="msgBody"
+                      onChange={onInputChange} 
+                    ></textarea>
+                    {
+                      (wasFormSubmited && msgBody.notValid) && <small className='contact__inputError'>{msgBody.notValid}</small>
+                    }
                   </div>
 
                   <button className='contact__submitButton' type='sumbit'>Enviar mensaje</button>
---
+++ src/hooks/useForm.js
@@ -0,0 +1,105 @@
+
+import { useMemo, useState } from 'react';
+
+export const useForm = (config = {}) => {
+
+    const formInitialValue = () => {
+        const newFormState = {}
+    
+        if(!!config && config.length > 0){
+            config.forEach(input => {
+                if(!input.name) throw new Error('useForm: Each entry in the config has to have a "name" property [String]')
+    
+                newFormState[input.name] = {};
+                newFormState[input.name].value = input.initialValue ? input.initialValue : '';
+                newFormState[input.name].notValid = undefined;
+            });
+        }
+    
+        return newFormState;
+    };
+
+    const initValidators = () => {
+        const newValidators = {}
+
+        if(!!config && config.length > 0){
+            config.forEach(input => {
+                if(input.regexValidators && input.regexValidators.length > 0){
+                    newValidators[input.name] = input.regexValidators;
+                }else{
+                    newValidators[input.name] = [];
+                }
+            })
+        }
+
+        return newValidators;
+    };
+
+    const [wasFormSubmited, setWasFormSubmited] = useState(false)
+    const [formState, setFormState] = useState(formInitialValue());
+    const validators = useMemo(() => initValidators(), []);
+    const isFormValid = useMemo(() => {
+        for(const inputName in formState){
+            if(formState[inputName].notValid !== undefined) return false;
+        }
+
+        return true;
+    }, [formState])
+
+
+    // TODO: Crear funciones onInputChange y onReset
+
+    const onInputChange = ({target}) => {
+
+        const notValidArr = validators[target.name].find(validator => !validator[0].test(target.value));
+
+        setFormState({
+            ...formState,
+            [target.name]: {
+                value: target.value,
+                notValid: notValidArr ? notValidArr[1] : undefined,
+            }
+        });
+    };
+
+    const validateAll = () => {
+        let newFormState = {...formState};
+
+        for(const inputName in formState){
+            const notValidArr = validators[inputName].find(validator => !validator[0].test(formState[inputName].value));
+
+            newFormState = {
+                ...newFormState,
+                [inputName]: {
+                    ...newFormState[inputName],
+                    notValid: notValidArr ? notValidArr[1] : undefined,
+                },
+            };
+        }
+
+        setFormState(newFormState);
+    }
+
+    const resetForm = () => {
+        setFormState(formInitialValue());
+        setWasFormSubmited(false);
+    }
+
+    const setFormSubmitted = () => {
+        setWasFormSubmited(true);
+        validateAll();
+    };
+
+
+    
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        isFormValid,
+        resetForm,
+        wasFormSubmited,
+        setFormSubmitted,
+    };
+};
